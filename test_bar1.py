8e21b577fb5b0665d8c57b0103fdc78f0c266ffc
Mikazinho <72050683+Velkhanna@users.noreply.github.com>
2022-03-29 16:22:05 -0300

    Add files via upload

---
+++ test_bar1.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestFOOBAR():
+   def bar_nao_executado():
+       pass
+   def test_another_foo_bar():
+       assert True
+
+class SeiLa():
+    def test_foo_bar_return():
+        assert True
