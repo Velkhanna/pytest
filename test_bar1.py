a330a1fd8f9d2826b6e012b4d4c3f43d547905fa
Mikazinho <72050683+Velkhanna@users.noreply.github.com>
2022-04-01 18:08:17 -0300

    Update test_bar1.py

--- test_bar1.py
+++ test_bar1.py
@@ -1,11 +1,8 @@
 import pytest
 
-class TestFOOBAR():
-   def bar_nao_executado():
-       pass
-   def test_another_foo_bar():
-       assert True
-
-class SeiLa():
-    def test_foo_bar_return():
-        assert True
+   def impar_ou_par(num):
+       '''
+       Este código irá retornar se é impar ou par
+       '''
+   def test_impar_ou_par():
+       assert impar_ou_par(2) == "Par"
